ac85177b5e7a8d10636795dc98381e403e5e8db6
Emmanuel Santiago <emmanuel.nearshore@gmail.com>
2023-03-28 22:24:53 -0600

    add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Navbar } from '@/components/Navbar'
 import Head from 'next/head'
 
 export default function Home() {
@@ -10,9 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <Navbar />
       </main>
     </>
   )
