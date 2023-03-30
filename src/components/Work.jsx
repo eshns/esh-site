e07d5758d2e735e66b1cf905d038273c882dcab8
Emmanuel Santiago <emmanuel.nearshore@gmail.com>
2023-03-30 17:09:10 -0600

    add skills section

---
+++ src/components/Work.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Link from 'next/link'
+import workImg from '../assets/projects/workImg.jpeg'
+import realState from '../assets/projects/realestate.jpg'
+
+export const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-[#F6683C]'>Work</p>
+                <p className='py-6'>{`// Check out of my recent work`}</p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div style={{backgroundImage: `url(${workImg.src})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React js Application
+                        </span>
+                        <div>
+                            <Link href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
+                            </Link>
+                            <Link href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{backgroundImage: `url(${realState.src})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React js Application
+                        </span>
+                        <div>
+                            <Link href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
+                            </Link>
+                            <Link href="/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Hero } from '@/components/Hero'
 import Head from 'next/head'
 import { About } from '@/components/About'
 import { Skills } from '@/components/Skills'
+import { Work } from '@/components/Work'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Hero />
         <About />
         <Skills />
+        <Work />
       </main>
     </>
   )
