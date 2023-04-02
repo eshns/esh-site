7f427c152b85bb84de480adf0796c6403308bebb
Emmanuel Santiago <emmanuel.nearshore@gmail.com>
2023-04-01 23:20:03 -0600

    add logo & favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
+import Logo from '../assets/logo.png';
 import Link from 'next/link'
+import Image from 'next/image';
 import {Link as InternalLink} from 'react-scroll' 
 
 export const Navbar = () => {
@@ -10,9 +12,9 @@ export const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[100px] flex justify-between items-center px-56 bg-[#0a192f] text-gray-300'>
         <div>
-            Logo
+            <Image src={Logo} alt='Logo Image' style={{ width: '150px', height:'150px' }} />
         </div>
         
         {/* menu */}
