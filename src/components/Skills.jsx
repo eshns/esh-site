fec8617504e63b9d96185dae15184f7498f2dea6
Emmanuel Santiago <emmanuel.nearshore@gmail.com>
2023-04-18 00:23:48 -0600

    remove background cards

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,42 +22,42 @@ export const Skills = () => {
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={HTML} alt="HTML ICON" width={80} height={80}/>
               <p className='my-4'>HTML</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={CSS} alt="CSS ICON" width={80} height={80}/>
               <p className='my-4'>CSS</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={JAVASCRIPT} alt="Javascript ICON" width={80} height={80}/>
               <p className='my-4'>JAVASCRIPT</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={REACTImg} alt="React ICON" width={80} height={80}/>
               <p className='my-4'>REACT</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={GitHub} alt="Github ICON" width={80} height={80}/>
               <p className='my-4'>GITHUB</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={NODE} alt="Nodejs ICON" width={80} height={80}/>
               <p className='my-4'>NODE JS</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={MONGO} alt="Mongodb ICON" width={80} height={80}/>
               <p className='my-4'>MONGO DB</p>
             </div>
 
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <div className='hover:scale-110 duration-500'>
               <Image className='mx-auto' src={AWS} alt="Aws ICON" width={80} height={80}/>
               <p className='my-4'>AWS</p>
             </div>
